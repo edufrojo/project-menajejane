ef69fa7f5f177195ab8671b84a861994841f81cb
edufrojo <edufrojo@gmail.com>
2020-06-09 16:13:02 +0200

    fix: actualizacion dinamica "ofertas"

--- src/app/components/ofertas/ofertas.component.ts
+++ src/app/components/ofertas/ofertas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Articulo } from '../../models/articulo.model';
 
 @Component({
   selector: 'app-ofertas',
@@ -7,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfertasComponent implements OnInit {
 
+  ofertas: Articulo[] = [
+    new Articulo('Producto 1','Lorem ipsum','https://source.unsplash.com/user/edufrojo/1600x901'),
+    new Articulo('Producto 2','Lorem ipsum','https://source.unsplash.com/user/edufrojo/1600x902'),
+    new Articulo('Producto 3','Lorem ipsum','https://source.unsplash.com/user/edufrojo/1600x903'),
+    new Articulo('Producto 4','Lorem ipsum','https://source.unsplash.com/user/edufrojo/1600x904'),
+    new Articulo('Producto 5','Lorem ipsum','https://source.unsplash.com/user/edufrojo/1600x905'),
+    new Articulo('Producto 6','Lorem ipsum','https://source.unsplash.com/user/edufrojo/1600x906'),
+  ];
   constructor() { }
 
   ngOnInit(): void {
