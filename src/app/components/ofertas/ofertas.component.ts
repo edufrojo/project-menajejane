62c231d9a0be510071f7ccf335679cd0d69e7cf6
edufrojo <edufrojo@gmail.com>
2020-06-09 18:54:31 +0200

    fix: descripciones ofertas

--- src/app/components/ofertas/ofertas.component.ts
+++ src/app/components/ofertas/ofertas.component.ts
@@ -9,12 +9,12 @@ import { Articulo } from '../../models/articulo.model';
 export class OfertasComponent implements OnInit {
 
   ofertas: Articulo[] = [
-    new Articulo('Producto 1','Lorem ipsum','https://source.unsplash.com/user/edufrojo/1600x901'),
-    new Articulo('Producto 2','Lorem ipsum','https://source.unsplash.com/user/edufrojo/1600x902'),
-    new Articulo('Producto 3','Lorem ipsum','https://source.unsplash.com/user/edufrojo/1600x903'),
-    new Articulo('Producto 4','Lorem ipsum','https://source.unsplash.com/user/edufrojo/1600x904'),
-    new Articulo('Producto 5','Lorem ipsum','https://source.unsplash.com/user/edufrojo/1600x905'),
-    new Articulo('Producto 6','Lorem ipsum','https://source.unsplash.com/user/edufrojo/1600x906'),
+    new Articulo('Producto 1','Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur vitae aperiam laborum odit minima quisquam suscipit rerum, harum reprehenderit perferendis sint?','https://source.unsplash.com/user/edufrojo/1600x901'),
+    new Articulo('Producto 2','Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur vitae aperiam laborum odit minima quisquam suscipit rerum, harum reprehenderit perferendis sint?','https://source.unsplash.com/user/edufrojo/1600x902'),
+    new Articulo('Producto 3','Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur vitae aperiam laborum odit minima quisquam suscipit rerum, harum reprehenderit perferendis sint?','https://source.unsplash.com/user/edufrojo/1600x903'),
+    new Articulo('Producto 4','Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur vitae aperiam laborum odit minima quisquam suscipit rerum, harum reprehenderit perferendis sint?','https://source.unsplash.com/user/edufrojo/1600x904'),
+    new Articulo('Producto 5','Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur vitae aperiam laborum odit minima quisquam suscipit rerum, harum reprehenderit perferendis sint?','https://source.unsplash.com/user/edufrojo/1600x905'),
+    new Articulo('Producto 6','Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur vitae aperiam laborum odit minima quisquam suscipit rerum, harum reprehenderit perferendis sint?','https://source.unsplash.com/user/edufrojo/1600x906'),
   ];
   constructor() { }
 
