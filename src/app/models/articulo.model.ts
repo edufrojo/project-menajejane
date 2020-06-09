bbedaf64f1586be123540b0a6cd53835d6091fb3
edufrojo <edufrojo@gmail.com>
2020-06-09 16:12:36 +0200

    feat: creacion del modelo "articulo"

---
+++ src/app/models/articulo.model.ts
@@ -0,0 +1,11 @@
+export class Articulo {
+  public nombre: string;
+  public descripcion: string;
+  public imagenRuta: string;
+
+  constructor(nombre: string, descripcion: string, imagenRuta: string) {
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.imagenRuta = imagenRuta;
+  }
+}
